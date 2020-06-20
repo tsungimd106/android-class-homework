d54fdcbd8d9d5c30bef0fc0193642978f8067772
chihyu <tsungimd106@gamil.com>
2020-06-20 15:49:31 +0800

    完成home畫面

--- ocr-reader-complete/app/src/main/java/com/google/android/gms/samples/vision/ocrreader/HomeActivity.java
+++ ocr-reader-complete/app/src/main/java/com/google/android/gms/samples/vision/ocrreader/HomeActivity.java
@@ -1,13 +1,93 @@
 package com.google.android.gms.samples.vision.ocrreader;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class HomeActivity extends AppCompatActivity {
-
+private Button add,unadd,times,into;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        add=findViewById(R.id.addButton);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("enter this");
+                chanegePage("+");
+            }
+        });
+        unadd =findViewById(R.id.button2);
+        unadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("enter this");
+                chanegePage("-");
+            }
+        });
+        times=findViewById(R.id.button3);
+        times.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("enter this");
+                chanegePage("×");
+            }
+        });
+        into=findViewById(R.id.button4);
+        into.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("enter this");
+                chanegePage("÷");
+            }
+        });
+    }
+    private void chanegePage(final String mode){
+        RadioGroup level = new RadioGroup(HomeActivity.this);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this).setTitle("詳細資料");
+        LinearLayout linearLayout = new LinearLayout(HomeActivity.this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setPadding(100, 0, 0, 0);
+        TextView textViewLable =new TextView(HomeActivity.this);
+        textViewLable.setText("123");
+        linearLayout.addView(textViewLable);
+        RadioButton high=new RadioButton(HomeActivity.this);
+        RadioButton low=new RadioButton(HomeActivity.this);
+        high.setText("十位數");
+        low.setText("個位數");
+        level.addView(high);
+        level.addView(low);
+        linearLayout.addView(level);
+        builder.setView(linearLayout);
+        builder.setNegativeButton("關閉", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        builder.setPositiveButton("確認", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent gonext = new Intent(HomeActivity.this,OcrCaptureActivity.class);
+                gonext.putExtra("mode",mode);
+//                gonext.putExtra("difficult",difficult);
+                startActivity(gonext);
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 }
